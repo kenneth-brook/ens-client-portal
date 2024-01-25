909909a8ea09ce579258dd6cee95115dfd6ce2fe
Kenneth Brook <kenneth.brook@hotmail.com>
2024-01-25 11:21:46 -0500

    animated left side bar added

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const sideMenu = document.getElementById("sideMenu");
 const contentBody = document.getElementById("contentBody");
+
+function toggleDiv() {
+    const container = document.querySelector('.container');
+    container.classList.toggle('open');
+    container.classList.toggle('rotated');
+}
