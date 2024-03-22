35bcc5a58a8ab109440f3ebffb2dadef11ee955c
Kenneth Brook <kenneth.brook@hotmail.com>
2024-03-22 16:50:13 -0400

    updates

--- api/clientData.js
+++ api/clientData.js
@@ -2,7 +2,9 @@ import { globalState } from '../reactive/state.js'
 
 export async function fetchClientData() {
   try {
-    const response = await fetch('URL_TO_YOUR_LAMBDA_FUNCTION_FOR_CLIENT_DATA')
+    const response = await fetch(
+      `https://client-control.911-ens-services.com/boot-strap-client/${userData.key}`,
+    )
     const data = await response.json()
     globalState.setState({ clientData: data })
   } catch (error) {
--- api/mainData.js
+++ api/mainData.js
@@ -2,7 +2,9 @@ import { globalState } from '../reactive/state.js'
 
 export async function fetchMainData() {
   try {
-    const response = await fetch('URL_TO_YOUR_LAMBDA_FUNCTION_FOR_MAIN_DATA')
+    const response = await fetch(
+      `https://matrix.911-ens-services.com/data/${userData.key}`,
+    )
     const data = await response.json()
     globalState.setState({ mainData: data })
   } catch (error) {
