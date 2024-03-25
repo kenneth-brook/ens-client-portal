90b87bb873a71de0e3933ff547346f34ea736262
Kenneth Brook <kenneth.brook@hotmail.com>
2024-03-25 09:14:11 -0400

    state management opperational

--- api/clientData.js
+++ api/clientData.js
@@ -2,13 +2,14 @@ import { globalState } from '../reactive/state.js'
 
 export async function fetchClientData() {
   console.log('clientData Launched')
-  try {
-    const response = await fetch(
-      `https://client-control.911-ens-services.com/boot-strap-client/${userData.key}`,
-    )
-    const data = await response.json()
-    globalState.setState({ clientData: data })
-  } catch (error) {
-    console.error('Error fetching client data:', error)
+  const response = await fetch(
+    `https://client-control.911-ens-services.com/boot-strap-client/${userData.key}`,
+  )
+  if (!response.ok) {
+    throw new Error('Failed to fetch main data')
   }
+  const data = await response.json() // Make sure to parse the JSON body
+  console.log('client data: ' + data)
+  globalState.setState({ mainData: data })
+  return data
 }
--- api/mainData.js
+++ api/mainData.js
@@ -1,14 +1,15 @@
 import { globalState } from '../reactive/state.js'
 
 export async function fetchMainData() {
-  try {
-    console.log('mainData Launched')
-    const response = await fetch(
-      `https://matrix.911-ens-services.com/data/${userData.key}`,
-    )
-    const data = await response.json()
-    globalState.setState({ mainData: data })
-  } catch (error) {
-    console.error('Error fetching main data:', error)
+  console.log('mainData Launched')
+  const response = await fetch(
+    `https://matrix.911-ens-services.com/data/${userData.key}`,
+  )
+  if (!response.ok) {
+    throw new Error('Failed to fetch main data')
   }
+  const data = await response.json() // Make sure to parse the JSON body
+  console.log('main data: ' + data)
+  globalState.setState({ mainData: data })
+  return data
 }
